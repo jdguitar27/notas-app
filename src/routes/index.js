722d5ffaa33fed2ab07adea541e73bd35fdf8da6
José <jose27duran08@gmail.com>
2018-12-24 18:43:02 -0430

    Configuración de rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('Index');
+    res.send('<h1>Index</h1>');
+});
+
+router.get('/about', (req, res) => {
+    res.send('<h1>¿Quién Soy?</h1>');
 });
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
 
+router.get('/singin', (req, res) => {
+    res.send('<h1>Ingresando a la aplicación</h1>');
+});
 
 module.exports = router;
