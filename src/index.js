35b715fa52cb9a9c1b359bed4d46fa0bd4660499
José <jose27duran08@gmail.com>
2018-12-24 17:43:30 -0430

    Configuración de motor de vistas 'express-handlebars'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+const methodOverride = require('method-override');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const express = require('express');
 const app = express();
 
-// Settings
+//////////////
+// Settings //
+//////////////
 
-app.set('port', process.env.port || 3000); // Configura el puesto en el que escuchará la aplicación
+app.set( 'port', process.env.port || 3000 ); // Configura el puesto en el que escuchará la aplicación
+app.set( 'views', path.join( __dirname, '/views' ) ); // Configura la dirección de las plantillas html
+// Configurando motor de plantillas handlebars
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join( app.get( 'views' ), 'layouts' ),
+    partialsDir: path.join( app.get( 'views' ), 'partials' ),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs')
 
-// Middlewares
+/////////////////
+// Middlewares //
+/////////////////
+
+app.use(express.urlencoded( {extended: false} ));
 
 // Global Variables
 
@@ -15,5 +33,5 @@ app.set('port', process.env.port || 3000); // Configura el puesto en el que escu
 
 // Server is listening
 app.listen(app.get('port'), () => {
-    console.log(`Servidor corriendo en ${app.get('port')}`);
+    console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
 });
